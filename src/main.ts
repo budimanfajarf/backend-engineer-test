3ee9a8d31495b8db6896ca7117205a87c67656db
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-17 17:47:03 +0700

    Add swagger, set global prefix, set users tag

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set global prefix to /api
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend Engineer Test')
+    .setDescription('Node.js (Nest.js) with TypeScript, TypeORM, Jest, MySQL')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
