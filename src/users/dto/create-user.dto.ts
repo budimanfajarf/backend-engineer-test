fe16992f93a9cc5227fe2f1bb46fa634c88a64f0
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-17 23:40:17 +0700

    Add password validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
 import { UserRole } from '../entities/user.entity';
-import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  Matches,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -10,6 +16,17 @@ export class CreateUserDto {
   @IsNotEmpty()
   email: string;
 
+  /**
+   * @see https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+   */
+  @Matches(
+    '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$',
+    undefined,
+    {
+      message:
+        'Minimum 8 characters, contains uppercase, lowercase, number, and symbol.',
+    },
+  )
   @IsNotEmpty()
   password: string;
 
